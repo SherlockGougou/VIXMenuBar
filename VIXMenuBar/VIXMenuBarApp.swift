d6ad3803e36cfcdb1095fafe53b65ea608b499bb
sherlock <qinglingou@gmail.com>
2025-10-30 13:01:37 +0800

    Initial Commit

---
+++ VIXMenuBar/VIXMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIXMenuBarApp.swift
+//  VIXMenuBar
+//
+//  Created by 勾清林 on 2025/10/30.
+//
+
+import SwiftUI
+
+@main
+struct VIXMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VIXMenuBarTests/VIXMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  VIXMenuBarTests.swift
+//  VIXMenuBarTests
+//
+//  Created by 勾清林 on 2025/10/30.
+//
+
+import Testing
+@testable import VIXMenuBar
+
+struct VIXMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
